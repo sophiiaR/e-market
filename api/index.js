fb7ecff8603355b12e5760fa080adf10def40d4b
Sofia <sraissiguier@gmail.com>
2021-11-29 13:17:47 -0300

    Change base url

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const PORT = process.env.PORT 
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+//axios.defaults.baseURL = "http://localhost:3001";
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
